c219486c6047ecf6fcd1f27f93946eb6b9bc87c1
Dias-Flow <ssu585036@gmail.com>
2026-04-02 14:41:40 +0100

    Add AbstractAccount base class

--- src/accounts.py
+++ src/accounts.py
@@ -1,6 +1,6 @@
 from abc import ABC, abstractmethod
-
-from enums import AccountStatus
+from uuid import uuid4
+from enums import AccountStatus, Currency
 
 
 class AbstractAccount(ABC):
@@ -21,3 +21,48 @@ class AbstractAccount(ABC):
     @abstractmethod
     def get_account_info(self):
         pass
+
+
+class BankAccount(AbstractAccount):
+    def __init__(
+        self,
+        owner: str,
+        balance: float = 0.0,
+        status: AccountStatus = AccountStatus.ACTIVE,
+        currency: Currency = Currency.RUB,
+        account_id: str | None = None
+    ):
+
+        if not owner or not owner.strip():
+            raise ValueError("Владелец счета не может быть пустым")
+
+        if balance < 0:
+            raise ValueError("Начальный баланс не может быть отрицательным")
+
+        if not isinstance(status, AccountStatus):
+            raise ValueError("Некорректный статус счета")
+
+        if not isinstance(currency, Currency):
+            raise ValueError("Некорректная валюта счета")
+
+        if account_id is None:
+            account_id = str(uuid4())[:8]
+
+        super().__init__(
+            account_id=account_id,
+            owner=owner,
+            balance=balance,
+            status=status
+        )
+
+
+        self.currency = currency
+
+    def get_account_info(self):
+        return {
+            "account_id": self.account_id,
+            "owner": self.owner,
+            "balance": self._balance,
+            "status": self.status.value,
+            "currency": self.currency.value
+        }
